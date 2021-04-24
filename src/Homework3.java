0291140fa92e11871cf9d38a7b5906b2e847b5ac
unknown <mariam1990hakobyan@gmail.com>
2021-04-24 15:23:23 +0400

    message

---
+++ src/Homework3.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+public class Homework3 {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+        int a = input.nextInt();
+        System.out.print("Input the second number: ");
+        int b = input.nextInt();
+        int c = (a / b);
+        System.out.println("The division of a and b is:" + c);
+    }
+
+
+
+
+    }
